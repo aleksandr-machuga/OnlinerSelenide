22bd2e03eb904e4677dd006cde12c2f1d4cc8d39
Aleksandr Machuga <machuga.aleksandr@gmail.com>
2022-11-25 15:27:25 +0300

    try to fix jenkins browser issue

--- src/main/java/by/it_academy/onliner/ui_framework/framework/LocalDriverFactory.java
+++ src/main/java/by/it_academy/onliner/ui_framework/framework/LocalDriverFactory.java
@@ -1,6 +1,8 @@
 package by.it_academy.onliner.ui_framework.framework;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static by.it_academy.onliner.ui_framework.framework.ConfigurationConstants.DEFAULT_BROWSER;
 import static io.github.bonigarcia.wdm.WebDriverManager.*;
@@ -12,7 +14,14 @@ public class LocalDriverFactory implements DriverFactory {
     public void setUp(String browser) {
         Configuration.browser = defaultIfNull(browser, DEFAULT_BROWSER);
         switch (defaultIfNull(browser, DEFAULT_BROWSER)) {
-            case ConfigurationConstants.EDGE_BROWSER -> edgedriver().setup();
+            case ConfigurationConstants.EDGE_BROWSER -> {
+                edgedriver().setup();
+                EdgeOptions options = new EdgeOptions();
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                options.addArguments("--headless");
+                capabilities.setCapability(EdgeOptions.CAPABILITY, options);
+                Configuration.browserCapabilities=capabilities;
+            }
             case ConfigurationConstants.FIREFOX_BROWSER -> firefoxdriver().setup();
             default -> chromedriver().setup();
         }
