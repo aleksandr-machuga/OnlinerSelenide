9739cd1f358265a3a36bf9b2fe264eec89fb7c70
Aleksandr Machuga <machuga.aleksandr@gmail.com>
2022-11-25 15:59:28 +0300

    try to fix jenkins browser issue

--- src/main/java/by/it_academy/onliner/ui_framework/framework/LocalDriverFactory.java
+++ src/main/java/by/it_academy/onliner/ui_framework/framework/LocalDriverFactory.java
@@ -1,8 +1,6 @@
 package by.it_academy.onliner.ui_framework.framework;
 
 import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static by.it_academy.onliner.ui_framework.framework.ConfigurationConstants.DEFAULT_BROWSER;
 import static io.github.bonigarcia.wdm.WebDriverManager.*;
@@ -13,18 +11,10 @@ public class LocalDriverFactory implements DriverFactory {
     @Override
     public void setUp(String browser) {
         Configuration.browser = defaultIfNull(browser, DEFAULT_BROWSER);
-        switch (defaultIfNull(browser, DEFAULT_BROWSER)) {
-            case ConfigurationConstants.EDGE_BROWSER -> {
-                edgedriver().setup();
-                EdgeOptions options = new EdgeOptions();
-                DesiredCapabilities capabilities = new DesiredCapabilities();
-                options.addArguments("--headless");
-                options.addArguments("--remote-debugging-port=<port>");
-                capabilities.setCapability(EdgeOptions.CAPABILITY, options);
-                Configuration.browserCapabilities=capabilities;
-            }
-            case ConfigurationConstants.FIREFOX_BROWSER -> firefoxdriver().setup();
-            default -> chromedriver().setup();
+        if (ConfigurationConstants.FIREFOX_BROWSER.equals(defaultIfNull(browser, DEFAULT_BROWSER))) {
+            firefoxdriver().setup();
+        } else {
+            chromedriver().setup();
         }
     }
 }
